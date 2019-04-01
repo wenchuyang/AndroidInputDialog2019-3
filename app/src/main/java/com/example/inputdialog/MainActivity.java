a42dcf5d8baf6346b49efb29c622080ddbf3d893
wenhuan <wenh@soundpayments.cn>
2019-04-01 14:48:34 +0800

    添加弹框以及简单的样式

--- app/src/main/java/com/example/inputdialog/MainActivity.java
+++ app/src/main/java/com/example/inputdialog/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.inputdialog;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import butterknife.BindView;
@@ -26,13 +32,50 @@ public class MainActivity extends AppCompatActivity {
     private void init(){
         show_button.setOnClickListener(onClick);
     }
+
     public View.OnClickListener onClick= new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             switch (v.getId()) {
                 case R.id.show_button:
-                    Toast.makeText(MainActivity.this, "show", Toast.LENGTH_SHORT).show();
+                    showDialog(MainActivity.this, "Input Dialog");
+                    break;
             }
         }
     };
+    private void showDialog(final Context context, final String title) {
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View promptView = layoutInflater.inflate(R.layout.dialog_input, null); //设置输入框样式
+        AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.AlertDialog); //设置弹框样式
+        builder.setView(promptView);
+
+        final EditText editText = (EditText) promptView.findViewById(R.id.input);
+
+        editText.post(new Runnable() { // 自动弹出输入法
+            @Override
+            public void run() {
+                editText.setFocusable(true);
+                editText.setFocusableInTouchMode(true);
+                editText.requestFocus();
+                InputMethodManager imm = (InputMethodManager)
+                        context.getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (imm != null) {
+                    imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT);
+                }
+            }
+        });
+
+        builder.setTitle(title); //弹框标题
+        builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "show", Toast.LENGTH_SHORT).show();
+            }
+        }); //右边的按钮
+        builder.setNegativeButton("Cancel", null); //左边的按钮
+        builder.setCancelable(false);  //点击阴影处对话框不会消失
+
+        builder.show();
+    }
+
 }
