d68645f650bbdeb160e95d45bcd3e71368f9b59d
wenhuan <wenh@soundpayments.cn>
2019-04-01 11:40:06 +0800

    1. modify icon 2. add BindView show_Button

--- app/src/main/java/com/example/inputdialog/MainActivity.java
+++ app/src/main/java/com/example/inputdialog/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.inputdialog;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.show_button) Button show_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        init();
+
     }
+
+    private void init(){
+        show_button.setOnClickListener(onClick);
+    }
+    public View.OnClickListener onClick= new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.show_button:
+                    Toast.makeText(MainActivity.this, "show", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
 }
